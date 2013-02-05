bb59909ce8c4f751927cb21e33d45a529582b5b5
David Adsit <david.adsit@gmail.com>
2013-02-04 17:00:09 -0700

    Update ThrowItOverTheWall.bat

--- ThrowItOverTheWall.bat
+++ ThrowItOverTheWall.bat
@@ -1,1 +1,1 @@
-packages/NUnit.Runners.2.6.2/tools/nunit-console.exe AcceptanceTests/bin/Debug/AcceptanceTests.dll -stoponerror -noresult -nologo -nodots -xml=errors.txt
+packages/NUnit.Runners.2.6.2/tools/nunit-console.exe AcceptanceTests/bin/Debug/AcceptanceTests.dll -noresult -nologo -nodots -xml=errors.txt
